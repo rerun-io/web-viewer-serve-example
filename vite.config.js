79fceec6b5403071fb7b8657bc380fd1f0cd1317
Adamo Carolli <adamoc@meta.com>
2024-11-27 12:26:37 -0500

    Update web-viewer to 0.20.1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,34 @@
 import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { searchForWorkspaceRoot } from "vite";
+import fs from "node:fs";
+import path from "node:path";
+
+const reloadOnSave = {
+  name: "full-reload-always",
+  handleHotUpdate({ server }) {
+    // by default, vite uses hmr which doesn't work well with imperative handles
+    server.ws.send({ type: "full-reload" });
+    return [];
+  },
+};
 
 /** @type {import("vite").UserConfig} */
 const config = {
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [wasm(), reloadOnSave],
+  optimizeDeps: {
+    exclude: process.env.NODE_ENV === "production" ? [] : ["@rerun-io/web-viewer"],
+  },
+  build: {
+    target: "esnext",
+  },
   server: {
-    port: 5173,
+    fs: {
+      allow: [
+        searchForWorkspaceRoot(process.cwd()),
+        // NOTE: hack to allow `new URL("file://...")` in `web-viewer` when it is a linked package
+        fs.realpathSync(path.join(__dirname, "node_modules", "@rerun-io/web-viewer")),
+      ],
+    },
   },
 };
 
